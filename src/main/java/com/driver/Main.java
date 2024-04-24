26687ac9dbb2953f09dcf739aff57d94c3388d2d
vikash-mdh <vikashkumar555550@gmail.com>
2024-04-24 11:49:03 +0530

    assiment complet

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
+class A{
+    public String meth(){
+        return "Invoking method from class A";
+    }
+
+}
+class B extends A{
+    @Override
+    public String meth(){
+        return "Method is overridden in Extendend class B";
+    }
+
+}
+
+
 public class Main {
-  
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.meth();
+    }
+
+
 }
